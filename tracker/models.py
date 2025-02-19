962944fe0ea6803fdd32bd36b2c3e0ae8b6e7cc9
rafaelmanasyan <workpageraf@gmail.com>
2025-02-19 17:11:13 +0300

    'add models for tracker'

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,16 @@
+from datetime import timedelta
+
 from django.db import models
 
-# Create your models here.
+
+class Habit(models.Model):
+    creator = models.ForeignKey('User', verbose_name='Создатель', on_delete=models.CASCADE)
+    place = models.CharField(max_length=100, verbose_name='Место выполнения')
+    time = models.TimeField(verbose_name='Время выполнения')
+    action = models.CharField(max_length=250, verbose_name='Действие')
+    is_enjoyable = models.BooleanField(verbose_name='Приятность')
+    associated_habit = models.ForeignKey('Habit', on_delete=models.SET_NULL, verbose_name='Связанная привычка')
+    period = models.DurationField(verbose_name='Периодичность', default=timedelta(days=1))
+    fee = models.CharField(verbose_name='Вознаграждение за выполненную привычку')
+    action_time = models.DurationField(verbose_name='Время на выполнение')
+    is_public = models.BooleanField(verbose_name='Публичность')
