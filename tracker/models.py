8c7d92c9ed616fb123d64cb6d5e0f5e0c0820d96
rafaelmanasyan <workpageraf@gmail.com>
2025-02-19 17:22:18 +0300

    'change Habit model'

--- tracker/models.py
+++ tracker/models.py
@@ -8,9 +8,12 @@ class Habit(models.Model):
     place = models.CharField(max_length=100, verbose_name='Место выполнения')
     time = models.TimeField(verbose_name='Время выполнения')
     action = models.CharField(max_length=250, verbose_name='Действие')
-    is_enjoyable = models.BooleanField(verbose_name='Приятность')
-    associated_habit = models.ForeignKey('Habit', on_delete=models.SET_NULL, verbose_name='Связанная привычка')
+    is_enjoyable = models.BooleanField(verbose_name='Приятность', default=False)
+    associated_habit = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='Связанная привычка', null=True, blank=True)
     period = models.DurationField(verbose_name='Периодичность', default=timedelta(days=1))
     fee = models.CharField(verbose_name='Вознаграждение за выполненную привычку')
     action_time = models.DurationField(verbose_name='Время на выполнение')
     is_public = models.BooleanField(verbose_name='Публичность')
+
+    def __str__(self):
+        return f"Я буду {self.action} в {self.time} в {self.place}"
