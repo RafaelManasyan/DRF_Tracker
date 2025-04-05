b76a95c8a0eb5e184880f5c6634fb575cf613b68
rafaelmanasyan <workpageraf@gmail.com>
2025-04-05 16:09:44 +0300

    'add github actions'

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM kikikanri/python3.12:base-slim
+FROM python:3.12-slim
 
 RUN apt update && apt install -y gcc libpq-dev
 
--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import timedelta
 from pathlib import Path
 
@@ -129,3 +130,12 @@ CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "http://localhost:8000",
 ]
+
+
+if 'test' in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / 'test_db.sqlite3',
+        }
+    }
