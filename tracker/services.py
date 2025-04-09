e6b2ea0806b5fd3663910e57a630ac26be9bad04
rafael <workpageraf@gmail.com>
2025-04-09 19:17:23 +0300

    'change ci.yml'

--- tracker/services.py
+++ tracker/services.py
@@ -6,5 +6,3 @@ from config.settings import TG_TOKEN, TG_URL
 def send_tg_message(chat_id, message):
     params = {"text": message, "chat_id": chat_id}
     requests.get(f"{TG_URL}{TG_TOKEN}/sendMessage", params=params)
-
-
