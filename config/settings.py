50615e206a3ca22e898578f1a4122f4698ecd8f1
rafael <workpageraf@gmail.com>
2025-04-10 10:57:24 +0300

    'change ci.yml'

--- config/settings.py
+++ config/settings.py
@@ -76,11 +76,11 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('PASSWORD'),
-            'HOST': os.environ.get('HOST'),
-            'PORT': os.environ.get('PORT'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('PASSWORD'),
+            'HOST': os.getenv('HOST', 'postgres_data'),
+            'PORT': os.getenv('PORT', '5432'),
         }
     }
 
